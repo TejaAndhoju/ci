58b4402855b407b6213ed25031a8ab51e3a3da5a
TejaAndhoju <andhojuteja@gmail.com>
2026-03-11 14:16:32 +0530

    :regression

---
+++ Test_model.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklelarn.linear_model import LinearRegression
+
+x=np.array([[1],[2],[3],[4]])
+y=np.array([2,4,6,8,10])
+
+def test_coefficients():
+    model=LinearRegression()
+    model.fit(x,y)
+    assert round(model.coef_[0],2)==2.00
+
+def test_intercept():
+    model=LinearRegression()
+    model.fit(x,y)
+    assert round(model.intercept_,2)==0.00
+
+def test_prediction():
+    model=LinearRegression()
+    model.fit(x,y)
+    prediction=model.predict([[5]])
+    assert round(prediction[0],2)==10.00
